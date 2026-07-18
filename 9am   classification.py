9fd673b3090d44b24b127b362dbbe84fd2f2e6c1
Sibaandajena <jenasibananda08@gmail.com>
2026-07-18 22:42:39 +0530

    ML Models

--- 9am   classification.py
+++ 9am   classification.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jul 16 10:08:36 2026
-
+Topic - Logistic regression with future prediction
 @author: SIBA
 """
 
@@ -17,8 +17,7 @@ y = dataset.iloc[:, -1].values
 
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test= train_test_split(X, y,
-                    test_size=20, random_state=0)
+X_train, X_test, y_train, y_test= train_test_split(X, y,test_size=20, random_state=0)
 
 
 from sklearn.preprocessing import StandardScaler
@@ -55,4 +54,50 @@ variance = classifier.score(X_test, y_test)
 print(variance)
 
 
+#---------------------FEATURE PREDICTIO-----------------------
+
+
+dataset1= pd.read_csv(r'C:\Users\HP\Desktop\ALL DATA SET\Future prediction1.csv')
+
+d2= dataset1.copy()
+
+dataset1= dataset1.iloc[:, [2, 3]].values
+
+from sklearn.preprocessing import StandardScaler
+sc= StandardScaler()
+M= sc.fit_transform(dataset1)
+
+y_pred1 = pd.DataFrame()
+
+
+d2['y_pred1']= classifier.predict(M)
+
+d2.to_csv('final1.csv')
+
+
+'''
+#  to get the path
+import os
+os.getcwd()
+'''
+
+# ******************************************************
+
+from sklearn.metrics import roc_auc_score, roc_curve
+y_pred_prob = classifier.predict_proba(X_test)[:, 1]
+
+auc_score= roc_auc_score(y_test, y_pred_prob)
+print(auc_score)
+
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+
+plt.Figure(figsize=(8,6))
+plt.plot(fpr, tpr, label=f'Logistic Regression (AUC= {auc_score:.2f})')
+plt.plot([0,1], [0,1], 'k--') # random classifier line
+plt.xlabel1('False Positive Rate')
+plt.ylabel1('True Positive Rate')
+plt.title('RDC Curve')
+plt.legend(loc='Lower right')
+plt.grid()
+plt.show()
 
